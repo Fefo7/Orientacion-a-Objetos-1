2fda7029e2cc430c4c1e6042fdca381a9fe3ed16
Federico Jose Isla <111016985+Fefo7@users.noreply.github.com>
2025-09-08 19:44:29 -0300

    Update Figura.java

--- Ejercicio7-Figuras-y-Cuerpos/src/main/java/ar/edu/unlp/info/oo1/Ejercicio5_Figuras_y_Cuerpos/Figura.java
+++ Ejercicio7-Figuras-y-Cuerpos/src/main/java/ar/edu/unlp/info/oo1/Ejercicio5_Figuras_y_Cuerpos/Figura.java
@@ -1,15 +1,8 @@
 package ar.edu.unlp.info.oo1.Ejercicio5_Figuras_y_Cuerpos;
 
-public abstract class  Figura {
-	private double perimetro;
-	private double area;
-	public void setPerimetro(double perimetro) {
-		this.perimetro = perimetro;
-	}
-	public void setArea(double area) {
-		this.area = area;
-	}
-	public abstract double getArea();
-	public abstract double getPerimetro();
+public interface  Figura {
+
+	public  double getArea();
+	public double getPerimetro();
 	
 }
