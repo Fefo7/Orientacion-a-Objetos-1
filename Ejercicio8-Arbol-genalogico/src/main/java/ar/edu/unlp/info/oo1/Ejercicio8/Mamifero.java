2107f274085f6af0a3f10a81728c899c9ea9405e
Federico Jose Isla <federicoisla1@gmail.com>
2025-09-15 21:50:35 -0300

    arregle problemas de encapsulamientos

---
+++ Ejercicio8-Arbol-genalogico/src/main/java/ar/edu/unlp/info/oo1/Ejercicio8/Mamifero.java
@@ -0,0 +1,99 @@
+package ar.edu.unlp.info.oo1.Ejercicio8;
+
+import java.time.LocalDate;
+
+public class Mamifero  {
+	
+	private String identificador;
+	private String especie;
+	private LocalDate fechaNacimiento;
+	private Mamifero padre;
+	private Mamifero madre;
+	
+	public Mamifero() {}
+	public Mamifero(String nombre) {
+		identificador = nombre;
+	}
+	
+	//getters y setters
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String id) {
+		this.identificador = id;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Mamifero getPadre() {
+		return padre;
+	}
+
+	public void setPadre(Mamifero padre) {
+		this.padre = padre;
+	}
+
+	public Mamifero getMadre() {
+		return madre;
+	}
+
+	public void setMadre(Mamifero madre) {
+		this.madre = madre;
+	}
+	
+	//@Comportamiento
+	public boolean tieneComoAncestroA(Mamifero unMamifero) {
+		
+		if (unMamifero.equals(this.identificador)) {
+	        return false;
+	    }
+		else 
+		{
+			return recorrerArbolGenialogico(unMamifero);
+		}
+		
+	    
+	}
+
+	public Mamifero getAbueloMaterno() { return (madre != null && madre.getPadre() != null) ? madre.getPadre() : null; }
+	public Mamifero getAbuelaMaterna() { return (madre != null && madre.getMadre() != null) ? madre.getMadre() : null; }
+	public Mamifero getAbueloPaterno() { return (padre != null && padre.getPadre() != null) ? padre.getPadre() : null; }
+	public Mamifero getAbuelaPaterna() { return (padre != null && padre.getMadre() != null) ? padre.getMadre() : null; }
+
+	private boolean recorrerArbolGenialogico(Mamifero unMamifero) 
+	{
+		if (unMamifero.equals(this.identificador)) { // rompo el encapsulamiento?
+	        return true;
+	    }
+		boolean ramaPadre = (this.getPadre() != null) && this.getPadre().recorrerArbolGenialogico(unMamifero);
+	    boolean ramaMadre = (this.getMadre() != null) && this.getMadre().recorrerArbolGenialogico(unMamifero);
+
+	    return ramaPadre || ramaMadre;
+	}
+	
+	private boolean equals(String id) 
+	{
+		return this.identificador == id;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Ejercicio9-Farola/src/main/java/ar/edu/unlp/info/oo1/Ejercicio9/Farola.java
@@ -0,0 +1,59 @@
+package ar.edu.unlp.info.oo1.Ejercicio9;
+
+import java.util.*;
+
+public class Farola {
+
+	private boolean isOn;
+	private List<Farola> vecinos =new ArrayList<>();;
+	public Farola() 
+	{
+		this.isOn = false;
+		
+	}
+	
+	public void pairWithNeighbor(Farola farola) 
+	{
+		this.agregarLista(farola);
+	    farola.agregarLista(this);
+	}
+	
+	public List<Farola> getNeighbors ()
+	{
+		return this.vecinos;
+	}
+	public void turnOn() 
+	{
+		if(!this.isOn) 
+		{
+			this.isOn = true;
+			this.vecinos.stream().forEach(v -> v.isOn = true);
+		}
+		
+	}
+	public void turnOff() 
+	{
+		if(this.isOn) 
+		{
+			this.isOn = false;
+			this.vecinos.stream().forEach(v -> v.isOn = false);
+		}
+		
+	}
+	
+	public boolean isOn() 
+	{
+		
+		return this.isOn;
+	}
+	public boolean isOff() 
+	{ 
+		
+		return !this.isOn;
+	}
+	private void agregarLista(Farola farola) 
+	{
+		this.vecinos.add(farola);
+	}
+
+}
